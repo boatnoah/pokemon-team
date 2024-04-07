c57a52c2fdf664c4564182cb6016170f9871a6de
boatnoah <noahskim7@gmail.com>
2024-04-06 18:17:03 -0700

    react, tailwind, shadcn setup, and homepage

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="font-bold text-4xl">Create your own Pokemon!</h1>
+        <h3>Here is where you can create your very own set of Pokemon!</h3>
+
+        <div className="flex justify-center">
+          <img
+            src="./src/assets/pikachu.jpeg"
+            className="w-[700px] h-[700px]"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
